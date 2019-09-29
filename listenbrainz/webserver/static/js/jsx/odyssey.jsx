737c9409c7277efba341fa38e55f1b4cab2cea74
Monkey Do <contact@monkeydo.digital>
2019-09-29 18:43:39 +0200

    Fix sort by distance

--- listenbrainz/webserver/static/js/jsx/odyssey.jsx
+++ listenbrainz/webserver/static/js/jsx/odyssey.jsx
@@ -118,7 +118,7 @@ class MusicalOdyssey extends React.Component {
     event.preventDefault();
     this.APIService.getSimilarTracksPlaylist(this.state.mbid0,this.state.metric,this.state.limit)
     //Do we need to order returned tracks by distance?
-    .then(listens => this.setState({listens: _.sortBy(listens || [], 'distance')}))
+    .then(listens => this.setState({listens: _.sortBy(listens || [], 'track_metadata.additional_info.distance')}))
     .catch(error => this.newAlert("danger",`Error (${error.status})`, error.message))
   }
   
