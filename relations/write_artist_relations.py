1be425ba92fbd33034ecb91d0ce59362b894412c
Robert Kaye <rob@musicbrainz.org>
2020-08-13 14:17:39 +0200

    Simplify the dump query since the ac contains the full string.

--- relations/write_artist_relations.py
+++ relations/write_artist_relations.py
@@ -24,15 +24,12 @@ DUMP_QUERY = '''SELECT CAST(count AS float) / (select max(count) from relations.
 
 DUMP_QUERY_AC = '''SELECT CAST(count AS float) / (SELECT MAX(count)
                                                     FROM relations.artist_credit_artist_credit_relations) AS score,
-                          ac0.id, string_agg(concat(acn0.name, acn0.join_phrase), ''),
-                          ac1.id, string_agg(concat(acn1.name, acn1.join_phrase), '')
+                          ac0.id, ac0.name, 
+                          ac1.id, ac1.name
                      FROM relations.artist_credit_artist_credit_relations arr
                      JOIN artist_credit ac0 ON arr.artist_credit_0 = ac0.id
-                     JOIN artist_credit_name acn0 ON arr.artist_credit_0 = acn0.artist_credit
                      JOIN artist_credit ac1 ON arr.artist_credit_1 = ac1.id
-                     JOIN artist_credit_name acn1 ON arr.artist_credit_1 = acn1.artist_credit
                     WHERE count > 3
-                 GROUP BY ac0.id, acn0.position, ac1.id, acn1.position, arr.count
                  ORDER BY score DESC
                 '''
 
